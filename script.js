750b920029485c0d996e30f5b556821dec0b549e
Michael Frandsen <mufc.michael@gmail.com>
2013-01-13 18:20:52 +0100

    asdf

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ $(window).load(function(){
 	// We are listening to the window.load event, so we can be sure
 	// that the images in the slideshow are loaded properly.
 
-
-	// Testing wether the current browser supports the canvas element:
-//	var supportCanvas = 'getContext' in document.createElement('canvas');
-
 	// The canvas manipulations of the images are CPU intensive,
 	// this is why we are using setTimeout to make them asynchronous
 	// and improve the responsiveness of the page.
@@ -19,19 +15,6 @@ $(window).load(function(){
 		
 		window.console && window.console.time && console.time('Generated In');
 		
-//		if(supportCanvas){
-//			$('#slideshow img').each(function(){
-
-//				if(!slideshow.width){
-					// Taking the dimensions of the first image:
-//					slideshow.width = this.width;
-//					slideshow.height = this.height;
-//				}
-				
-				// Rendering the modified versions of the images:
-//				createCanvasOverlay(this);
-//			});
-//		}
 		
 		window.console && window.console.timeEnd && console.timeEnd('Generated In');
 		
@@ -52,19 +35,8 @@ $(window).load(function(){
 
 			var next = slides.eq(nextIndex);
 			
-//			if(supportCanvas){
-//				canvas.fadeIn(750, function(){
-//					next.addClass('slideActive').fadeOut(0).fadeIn(750);
-//					li.fadeOut(750, function(){
-//						li.removeClass('slideActive');
-//						canvas.hide();
-//					});
-//				});
-//			}
-//			else {
 				next.addClass('slideActive').fadeOut(0).fadeIn(750);
 				li.removeClass('slideActive').fadeIn(0).fadeOut(750);
-//			}
 
 			current = nextIndex;
 		});
